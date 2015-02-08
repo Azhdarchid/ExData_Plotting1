c5e2408b36f84b30406ebf92bd451f1e43379bec
Des <dlteter@gmail.com>
2015-02-08 13:02:32 -0600

    Final

--- plot3.R
+++ plot3.R
@@ -20,7 +20,7 @@ with(subsetData, {
 })
 
 legend('topright', col=c('black', 'red', 'blue'), lty=1, lwd=2, 
-       legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
+       legend=c('Sub_metering_1 ', 'Sub_metering_2 ', 'Sub_metering_3 '), cex=.8)
 
 dev.copy(png, file='plot3.png', height=480, width=480)
 
