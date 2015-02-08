d9048ee4acf0b5c373a6073edc9ba6a3103baece
Des <dlteter@gmail.com>
2015-02-08 12:47:27 -0600

    Final

--- plot1.R
+++ plot1.R
@@ -1,13 +1,13 @@
 ## Read power consumption data
-readData <- read.table('household_power_consumption.txt', header=T, sep=';', 
-na.strings='?', quote='\"')
+readData <- read.table('household_power_consumption.txt', header=TRUE, sep=';', 
+                       na.strings='?', quote='\"')
 ## Subset data
 subsetData <- subset(readData, subset=(Date == '1/2/2007' | Date == '2/2/2007'))
 
 rm(readData)
 ## Create and save histogram
 hist(subsetData$Global_active_power, main='Global Active Power', 
-xlab='Global Active Power (kilowatts)', ylab='Frequency', col='Red')
+     xlab='Global Active Power (kilowatts)', ylab='Frequency', col='Red')
 
 dev.copy(png, file='plot1.png', height=480, width=480)
 
